22f833f60802ca24ed0104dc795f9f0ca43d1848
lailj <lailj@hytch.com>
2021-05-13 17:07:05 +0800

    修改Logger保存本地文件的最大数量为10个

--- NetStatusLibrary/src/main/java/com/llj/netstatuslibrary/log/DiskLogStrategy.java
+++ NetStatusLibrary/src/main/java/com/llj/netstatuslibrary/log/DiskLogStrategy.java
@@ -10,6 +10,9 @@ import androidx.annotation.Nullable;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 import static com.llj.netstatuslibrary.log.Utils.checkNotNull;
 
@@ -39,6 +42,7 @@ public class DiskLogStrategy implements LogStrategy {
 
     @NonNull private final String folder;
     private final int maxFileSize;
+    private int maxFileCount = 10;
 
     WriteHandler(@NonNull Looper looper, @NonNull String folder, int maxFileSize) {
       super(checkNotNull(looper));
@@ -84,19 +88,49 @@ public class DiskLogStrategy implements LogStrategy {
       fileWriter.append(content);
     }
 
-    private File getLogFile(@NonNull String folderName, @NonNull String fileName) {
-      checkNotNull(folderName);
-      checkNotNull(fileName);
+        private File getLogFile(@NonNull String folderName, @NonNull String fileName) {
+            checkNotNull(folderName);
+            checkNotNull(fileName);
 
-      File folder = new File(folderName);
-      if (!folder.exists()) {
-        //TODO: What if folder is not created, what happens then?
-        folder.mkdirs();
-      }
+            File folder = new File(folderName);
+            if (!folder.exists()) {
+                //TODO: What if folder is not created, what happens then?
+                folder.mkdirs();
+            }
+            //文件获取排序
+            File[] files = folder.listFiles();
+            List<File> fileList = Arrays.asList(files);
+            Arrays.sort(files, new Comparator<File>() {
+                public int compare(File f1, File f2) {
+                    long diff = f1.lastModified() - f2.lastModified();
+                    if (diff > 0)
+                        return 1;
+                    else if (diff == 0)
+                        return 0;
+                    else
+                        return -1;//如果 if 中修改为 返回-1 同时此处修改为返回 1  排序就会是递减
+                }
+            });
 
-      int newFileCount = 0;
-      File newFile;
-      File existingFile = null;
+            //超出限定数量文件删除
+            int deleCount = fileList.size() - maxFileCount;
+            if (deleCount > 0) {
+                for (int i = 0; i < deleCount; i++) {
+                    fileList.get(i).delete();
+                }
+            }
+
+            //查找文件列表中数值最大的文件
+            int newFileCount = 0;
+            if (fileList.size() > 0) {
+                String Path = fileList.get(fileList.size() - 1).getAbsolutePath();
+                String[] count = Path.split("_");
+                String[] split2 = count[1].split("\\.");
+                newFileCount = Integer.parseInt(split2[0]);
+            }
+
+            File newFile;
+            File existingFile = null;
 
       newFile = new File(folder, String.format("%s_%s.csv", fileName, newFileCount));
       while (newFile.exists()) {
