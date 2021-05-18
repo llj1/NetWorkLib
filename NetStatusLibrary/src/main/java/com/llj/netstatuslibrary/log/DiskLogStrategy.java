75ca6b2a9fdd927ef6a98434e65664210315da57
lailj <lailj@hytch.com>
2021-05-18 14:32:30 +0800

    5.0以下设备返回cache目录不同导致的问题

--- NetStatusLibrary/src/main/java/com/llj/netstatuslibrary/log/DiskLogStrategy.java
+++ NetStatusLibrary/src/main/java/com/llj/netstatuslibrary/log/DiskLogStrategy.java
@@ -20,73 +20,77 @@ import static com.llj.netstatuslibrary.log.Utils.checkNotNull;
 /**
  * Abstract class that takes care of background threading the file log operation on Android.
  * implementing classes are free to directly perform I/O operations there.
- *
+ * <p>
  * Writes all logs to the disk with CSV format.
  */
 public class DiskLogStrategy implements LogStrategy {
 
-  @NonNull private final Handler handler;
+    @NonNull
+    private final Handler handler;
 
-  public DiskLogStrategy(@NonNull Handler handler) {
-    this.handler = checkNotNull(handler);
-  }
-
-  @Override public void log(int level, @Nullable String tag, @NonNull String message) {
-    checkNotNull(message);
-
-    // do nothing on the calling thread, simply pass the tag/msg to the background thread
-    handler.sendMessage(handler.obtainMessage(level, message));
-  }
-
-  static class WriteHandler extends Handler {
-
-    @NonNull private final String folder;
-    private final int maxFileSize;
-    private int maxFileCount = 10;
-
-    WriteHandler(@NonNull Looper looper, @NonNull String folder, int maxFileSize) {
-      super(checkNotNull(looper));
-      this.folder = checkNotNull(folder);
-      this.maxFileSize = maxFileSize;
+    public DiskLogStrategy(@NonNull Handler handler) {
+        this.handler = checkNotNull(handler);
     }
 
-    @SuppressWarnings("checkstyle:emptyblock")
-    @Override public void handleMessage(@NonNull Message msg) {
-      String content = (String) msg.obj;
+    @Override
+    public void log(int level, @Nullable String tag, @NonNull String message) {
+        checkNotNull(message);
 
-      FileWriter fileWriter = null;
-      File logFile = getLogFile(folder, "logs");
+        // do nothing on the calling thread, simply pass the tag/msg to the background thread
+        handler.sendMessage(handler.obtainMessage(level, message));
+    }
 
-      try {
-        fileWriter = new FileWriter(logFile, true);
+    static class WriteHandler extends Handler {
 
-        writeLog(fileWriter, content);
+        @NonNull
+        private final String folder;
+        private final int maxFileSize;
+        private int maxFileCount = 10;
 
-        fileWriter.flush();
-        fileWriter.close();
-      } catch (IOException e) {
-        if (fileWriter != null) {
-          try {
-            fileWriter.flush();
-            fileWriter.close();
-          } catch (IOException e1) { /* fail silently */ }
+        WriteHandler(@NonNull Looper looper, @NonNull String folder, int maxFileSize) {
+            super(checkNotNull(looper));
+            this.folder = checkNotNull(folder);
+            this.maxFileSize = maxFileSize;
         }
-      }
-    }
 
-    /**
-     * This is always called on a single background thread.
-     * Implementing classes must ONLY write to the fileWriter and nothing more.
-     * The abstract class takes care of everything else including close the stream and catching IOException
-     *
-     * @param fileWriter an instance of FileWriter already initialised to the correct file
-     */
-    private void writeLog(@NonNull FileWriter fileWriter, @NonNull String content) throws IOException {
-      checkNotNull(fileWriter);
-      checkNotNull(content);
+        @SuppressWarnings("checkstyle:emptyblock")
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            String content = (String) msg.obj;
 
-      fileWriter.append(content);
-    }
+            FileWriter fileWriter = null;
+            File logFile = getLogFile(folder, "logs");
+
+            try {
+                fileWriter = new FileWriter(logFile, true);
+
+                writeLog(fileWriter, content);
+
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                if (fileWriter != null) {
+                    try {
+                        fileWriter.flush();
+                        fileWriter.close();
+                    } catch (IOException e1) { /* fail silently */ }
+                }
+            }
+        }
+
+        /**
+         * This is always called on a single background thread.
+         * Implementing classes must ONLY write to the fileWriter and nothing more.
+         * The abstract class takes care of everything else including close the stream and catching IOException
+         *
+         * @param fileWriter an instance of FileWriter already initialised to the correct file
+         */
+        private void writeLog(@NonNull FileWriter fileWriter, @NonNull String content) throws IOException {
+            checkNotNull(fileWriter);
+            checkNotNull(content);
+
+            fileWriter.append(content);
+        }
 
         private File getLogFile(@NonNull String folderName, @NonNull String fileName) {
             checkNotNull(folderName);
@@ -125,28 +129,28 @@ public class DiskLogStrategy implements LogStrategy {
             if (fileList.size() > 0) {
                 String Path = fileList.get(fileList.size() - 1).getAbsolutePath();
                 String[] count = Path.split("_");
-                String[] split2 = count[1].split("\\.");
+                String[] split2 = count[count.length - 1].split("\\.");
                 newFileCount = Integer.parseInt(split2[0]);
             }
 
             File newFile;
             File existingFile = null;
 
-      newFile = new File(folder, String.format("%s_%s.csv", fileName, newFileCount));
-      while (newFile.exists()) {
-        existingFile = newFile;
-        newFileCount++;
-        newFile = new File(folder, String.format("%s_%s.csv", fileName, newFileCount));
-      }
+            newFile = new File(folder, String.format("%s_%s.csv", fileName, newFileCount));
+            while (newFile.exists()) {
+                existingFile = newFile;
+                newFileCount++;
+                newFile = new File(folder, String.format("%s_%s.csv", fileName, newFileCount));
+            }
 
-      if (existingFile != null) {
-        if (existingFile.length() >= maxFileSize) {
-          return newFile;
+            if (existingFile != null) {
+                if (existingFile.length() >= maxFileSize) {
+                    return newFile;
+                }
+                return existingFile;
+            }
+
+            return newFile;
         }
-        return existingFile;
-      }
-
-      return newFile;
     }
-  }
 }
